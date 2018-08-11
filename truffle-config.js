4c3b6361804d2273322eb59e782e7c42f6a794b0
odan <odan3240@gmail.com>
2018-08-11 11:48:12 +0900

    exampleからコピペ

---
+++ truffle-config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  // See <http://truffleframework.com/docs/advanced/configuration>
+  // to customize your Truffle configuration!
+  // or just copy the configuration on truffle.js here
+};
